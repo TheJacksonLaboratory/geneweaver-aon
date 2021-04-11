7b1c40af459df30712ffca855b6e5fb7dedd781d
Sophie Kearney <sophie_kearney1@baylor.edu>
2021-04-10 22:04:26 -0500

    endpoint OrthologToFromSpecies to be reviewed

---
+++ flask/alembic/versions/bc972ff277e3_add_id_column.py
@@ -0,0 +1,24 @@
+"""add id column
+
+Revision ID: bc972ff277e3
+Revises: 4297df5638d7
+Create Date: 2021-04-10 17:16:32.692859
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'bc972ff277e3'
+down_revision = '4297df5638d7'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
